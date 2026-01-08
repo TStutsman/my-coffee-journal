738d7f8532505be59b604cfd7e69f63b0316c10c
TStutsman <teagandstutsman@gmail.com>
2026-01-08 10:37:39 -0500

    added brew-details component and migrated brew buttons

---
+++ react-vite/src/features/brews/BrewDetails.jsx
@@ -0,0 +1,98 @@
+import DeleteBrew from '@brews/DeleteBrew';
+import BrewForm from '@brews/BrewForm';
+import { useModal, useStore } from '@context';
+import { formatDate } from '@utils';
+import './BrewDetails.css'
+
+function BrewDetails({ brew, setBrew }) {
+    const { setOnModalClose, setModalContent } = useModal();
+    const { fetchBrews } = useStore();
+
+    // parsed grindSize and ratio (no trailing zeros)
+    const pGrindSize = Number(brew.grindSize);
+    const pRatio = Number(brew.ratio);
+    const pDose = Number(brew.dose);
+    const pWater = Number(brew.waterAmt);
+    const pRating = Number(brew.rating)
+    const pDate = formatDate(brew.date);
+
+    const editBrew = () => {
+        setModalContent(<BrewForm brewId={brew.id} />);
+        setOnModalClose(() => () => {
+            fetch(`/api/brews/${brew.id}`)
+            .then(res => res.json())
+            .then(data => formatObject(data))
+            .then(data => setBrew(data))
+        });
+    }
+    
+    const deleteBrew = () => {
+        fetch(`/api/brews/${brew.id}`,
+            {
+                method: 'DELETE',
+                credentials: 'include'
+            }
+        ).then(res => {
+            if(res.ok) fetchBrews();
+        });
+    }
+
+    const openConfirmDelete = () => {
+        setModalContent(<DeleteBrew deleteFn={deleteBrew} />)
+    }
+
+    return (
+        <div id="brew-details-modal">
+            <div className='brew-title'>
+                <div>
+                    <h4>{pDate}</h4>
+                    <h5>{pRating + '/5'}</h5>
+                </div>
+                <h5>{brew.brewer}</h5>
+            </div>
+
+            <div className='brew-content'>
+                    <div>
+                        <p style={{color: `var(--text-${brew.coffee.color})`}}>{brew.coffee.roaster} {brew.coffee.farm}</p>
+                    </div>
+                    <div className='content-box'>
+                        <span>{brew.grinder}</span>
+                    </div>
+                    <div className='content-box'>
+                        <div>
+                            <span>{pDose}g coffee</span>
+                            { brew.grindSize && <span className='secondary-details'>grind setting {pGrindSize}</span> }
+                        </div>
+                        <div>
+                            <span>{pWater}g water</span>
+                            { brew.waterTemp && <span className='secondary-details'>{brew.waterTemp}&deg;{brew.celsius ? "C" : "F"}</span> }
+                        </div>
+                        <div>
+                            <span>1/{pRatio} ratio</span>
+                        </div>
+                    </div>
+                    <div className='content-box'>
+                        <p>Recipe:&#10;{brew.recipe}</p>
+                    </div>
+                    <div className='content-box'>
+                        <p>Notes:&#10;{brew.notes}</p>
+                    </div>
+            </div>
+
+            <div className='brew-buttons'>
+                <button className='edit-brew-button' onClick={editBrew}>
+                    <svg className='edit-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill='currentColor'>
+                        <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-96c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 64z"/>
+                    </svg>
+                </button>
+                <button className='delete-brew-button' onClick={openConfirmDelete}>
+                    <svg className='trash-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill='currentColor'>
+                        <path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default BrewDetails;
