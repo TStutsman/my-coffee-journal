6bfa3eb239bd7a24afa605d1f08387e52b487a32
TStutsman <teagandstutsman@gmail.com>
2026-01-08 13:39:45 -0500

    try using getenv in app config

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config():
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_COOKIE_PATH = '/'
     SESSION_COOKIE_DOMAIN = '127.0.0.1' if os.getenv('FLASK_DEBUG') is True else None
